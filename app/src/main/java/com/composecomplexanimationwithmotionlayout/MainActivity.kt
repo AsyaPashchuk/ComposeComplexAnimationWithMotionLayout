96c5947635858282b1f2d742585a2036db9578f7
Viacheslav Panasiuk <badgerle82@gmail.com>
2022-11-30 13:18:59 +0200

    Motion_scene was added and implemented

--- app/src/main/java/com/composecomplexanimationwithmotionlayout/MainActivity.kt
+++ app/src/main/java/com/composecomplexanimationwithmotionlayout/MainActivity.kt
@@ -3,22 +3,38 @@ package com.composecomplexanimationwithmotionlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Slider
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import com.composecomplexanimationwithmotionlayout.composable.ProfileHeader
 import com.composecomplexanimationwithmotionlayout.ui.theme.ComposeComplexAnimationWithMotionLayoutTheme
 
+@ExperimentalMotionApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeComplexAnimationWithMotionLayoutTheme {
+                Column {
 
+                    var progressState by remember {
+                        mutableStateOf(0f)
+                    }
+                    
+                    ProfileHeader(progress = progressState)
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Slider(
+                        value = progressState,
+                        onValueChange = {progressState = it},
+                        modifier = Modifier.padding(horizontal = 32.dp)
+                    )
+                }
             }
         }
     }
 }
+
+
---
+++ app/src/main/java/com/composecomplexanimationwithmotionlayout/composable/ProfileHeader.kt
@@ -0,0 +1,67 @@
+package com.composecomplexanimationwithmotionlayout.composable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
+import com.composecomplexanimationwithmotionlayout.R
+
+@ExperimentalMotionApi
+@Composable
+fun ProfileHeader(progress: Float) {
+    val context = LocalContext.current
+    val motionScene = remember {
+        context.resources
+            .openRawResource(R.raw.motion_scene)
+            .readBytes()
+            .decodeToString()
+    }
+    MotionLayout(
+        motionScene = MotionScene(content = motionScene),
+        progress = progress,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        //Get the custom property from motion_scene.json5
+        val properties = motionProperties(id = "profile_pic")
+
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.DarkGray)
+            .layoutId("box")
+        )
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(CircleShape)
+                .border(
+                    width = 2.dp,
+                    color = properties.value.color("background"),
+                    shape = CircleShape
+                )
+                .layoutId("profile_pic")
+        )
+        Text(
+            text = "Asya",
+            fontSize = 16.sp,
+            modifier = Modifier.layoutId("user_name"),
+            color = properties.value.color("background")
+        )
+    }
+}
